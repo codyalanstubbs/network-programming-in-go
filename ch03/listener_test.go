71fa7fa75a1473849fd111fa49ce4db1ab07f2c5
Cody Alan Stubbs <castubbs3@gmail.com>
2024-12-26 18:39:49 -0500

    Format

--- ch03/listener_test.go
+++ ch03/listener_test.go
@@ -171,59 +171,59 @@ func TestDialContextCancel(t *testing.T) {
 }
 
 func TestDialContextCancelFanOut(t *testing.T) {
-    ctx, cancel := context.WithDeadline(
-        context.Background(),
-        time.Now().Add(10*time.Second),
-        )
+	ctx, cancel := context.WithDeadline(
+		context.Background(),
+		time.Now().Add(10*time.Second),
+	)
 
-    listener, err := net.Listen("tcp", "127.0.0.1:")
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer listener.Close()
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
 
-    go func() {
-        // Only accepting a single connection.
-        conn, err := listener.Accept()
-        if err == nil {
-            conn.Close()
-        }
-    }()
+	go func() {
+		// Only accepting a single connection.
+		conn, err := listener.Accept()
+		if err == nil {
+			conn.Close()
+		}
+	}()
 
-    dial := func(ctx context.Context, address string, response chan int, id int, wg *sync.WaitGroup) {
-        defer wg.Done()
+	dial := func(ctx context.Context, address string, response chan int, id int, wg *sync.WaitGroup) {
+		defer wg.Done()
 
-        var d net.Dialer
-        c, err := d.DialContext(ctx, "tcp", address)
-        if err != nil {
-            return
-        }
-        c.Close()
+		var d net.Dialer
+		c, err := d.DialContext(ctx, "tcp", address)
+		if err != nil {
+			return
+		}
+		c.Close()
 
-        select {
-        case  <-ctx.Done():
-        case response <- id:
-        }
+		select {
+		case <-ctx.Done():
+		case response <- id:
+		}
 
-    }
+	}
 
-    res := make(chan int)
-    var wg sync.WaitGroup
+	res := make(chan int)
+	var wg sync.WaitGroup
 
-    for i := 0; i < 10; i++ {
-        wg.Add(1)
-        go dial(ctx, listener.Addr().String(), res, i+1, &wg)
-    }
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go dial(ctx, listener.Addr().String(), res, i+1, &wg)
+	}
 
-    response := <-res
-    cancel()
-    wg.Wait()
-    close(res)
+	response := <-res
+	cancel()
+	wg.Wait()
+	close(res)
 
-    if ctx.Err() != context.Canceled {
-        t.Errorf("expected canceled context; actual: %s", ctx.Err())
-    }
+	if ctx.Err() != context.Canceled {
+		t.Errorf("expected canceled context; actual: %s", ctx.Err())
+	}
 
-    t.Logf("dialer %d retrieved the resource", response)
+	t.Logf("dialer %d retrieved the resource", response)
 
 }
