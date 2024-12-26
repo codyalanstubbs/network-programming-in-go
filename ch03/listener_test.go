948439b023eae954790d2248b1db38c09c62ab7e
Cody Alan Stubbs <castubbs3@gmail.com>
2024-12-26 17:09:39 -0500

    Use dial context

--- ch03/listener_test.go
+++ ch03/listener_test.go
@@ -1,6 +1,7 @@
 package ch03
 
 import (
+	"context"
 	"io"
 	"net"
 	"syscall"
@@ -107,4 +108,33 @@ func TestDialTimeout(t *testing.T) {
 	if !nErr.Timeout() {
 		t.Fatal("error is not a timeout")
 	}
+}
+
+func TestDialContext(t *testing.T) {
+    dl := time.Now().Add(5 * time.Second)
+    ctx, cancel := context.WithDeadline(context.Background(), dl)
+    defer cancel()
+
+    var d net.Dialer // DialContext is a method on a Dialer
+    d.Control = func(_, _ string, _ syscall.RawConn) error {
+        // Sleep long enough to reach the context's deadline.
+        time.Sleep(5*time.Second + time.Millisecond)
+        return nil
+    }
+    conn, err := d.DialContext(ctx, "tcp", "10.0.0.0:80")
+    if err == nil {
+        conn.Close()
+        t.Fatal("connection dsid not time out")
+    }
+    nErr, ok := err.(net.Error)
+    if !ok {
+        t.Error(err)
+    } else {
+        if !nErr.Timeout() {
+            t.Errorf("error is not a timeout: %v", err)
+        }
+    }
+    if ctx.Err() != context.DeadlineExceeded {
+        t.Errorf("expected deadline exceeded; actual: %v", ctx.Err())
+    }
 }
